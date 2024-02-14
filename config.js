265c6071bd54463c09c43ac44e1b07c424a240ad
Kiranjit Nagra <knagra873@gmail.com>
2024-02-13 21:10:14 -0500

    Updated config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 let config = {
   host    : 'ec2-3-137-65-169.us-east-2.compute.amazonaws.com',
-  user    : '',
-  password: '',
-  database: ''
+  user    : 'kknagra',
+  password: 'MSCI342',
+  database: 'kknagra'
 };
  
 export default config;
