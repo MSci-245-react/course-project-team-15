ae2f8e6c4d53f085bdf9c7e0058d0a7b6982c2cc
Kiranjit Nagra <knagra873@gmail.com>
2024-02-18 17:22:56 -0500

    created pages

---
+++ client/src/components/App/FeedPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Feed() {
+  return (
+    <div>
+      <h1>Feed</h1>
+      <p>See the latest reviews and posts from the community.</p>
+    </div>
+  );
+}
+
+export default Feed;
---
+++ client/src/components/App/Recommendations.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Recommendation() {
+  return (
+    <div>
+      <h1>Recommendations</h1>
+      <p>Your personalized restaurant recommendations.</p>
+    </div>
+  );
+}
+
+export default Recommendation;
