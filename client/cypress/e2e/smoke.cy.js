955a2abe63e463f30edb833efae91e8fae4c1c1f
Mason Au <masonau@Masons-MacBook-Air.local>
2024-02-23 16:24:32 -0500

    create integrated cypress test for profile page

--- client/cypress/e2e/smoke.cy.js
+++ client/cypress/e2e/smoke.cy.js
@@ -1,6 +1,53 @@
+/*
 describe('empty spec', () => {
   it('can view the home page', () => {
     cy.visit('/');
     cy.contains('Welcome to TasteOfLoo');
+  });
+});
+*/
+
+describe('Profile Page', () => {
+  beforeEach(() => {
+    // Visit the profile page before each test
+    cy.visit('/ProfilePage');
+  });
+
+  it('should display user information', () => {
+    // Assert that the user name and bio are displayed
+    cy.contains("User's Name").should('exist');
+    cy.contains("A brief bio about the user.").should('exist');
+  });
+
+  it('should display correct counts', () => {
+    // Assert that the counts for reviews, friends, visited, shortlisted, and favourites are displayed
+    cy.contains('Reviews: 2').should('exist');
+    cy.contains('Visited: 0').should('exist');
+    cy.contains('Shortlisted: 0').should('exist');
+    cy.contains('Favourited: 0').should('exist');
+    cy.contains('Friends: 2').should('exist');
+  });
+
+  it('should navigate to lists', () => {
+    // Click on each button and assert that the correct list page is visited
+    cy.contains('My Reviews').click();
+    cy.url().should('include', '/ReviewsList');
+    cy.go('back');
+
+    cy.contains('Been').click();
+    cy.url().should('include', '/BeenToList');
+    cy.go('back');
+
+    cy.contains('Want To Try').click();
+    cy.url().should('include', '/WantToTryList');
+    cy.go('back');
+
+    cy.contains('Favourites').click();
+    cy.url().should('include', '/FavouritesList');
+    cy.go('back');
+
+    cy.contains('My Friends').click();
+    cy.url().should('include', '/friends');
+    cy.go('back');
   });
 });
