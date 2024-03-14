71455b51ac8a4fe25b9e7f8f5419b6a43804df70
Kiranjit Nagra <knagra873@gmail.com>
2024-03-14 18:57:30 -0400

    created beentolist unit tests

---
+++ client/src/components/App/BeenToList.test.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { BrowserRouter as Router } from 'react-router-dom';
+import BeenToList from './BeenToList';
+import fetchMock from 'jest-fetch-mock';
+
+fetchMock.enableMocks();
+
+const mockedNavigate = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: () => mockedNavigate,
+}));
+
+  describe('BeenToList', () => {
+    beforeEach(() => {
+        mockedNavigate.mockClear();
+        localStorage.clear();
+        fetch.resetMocks();
+    });
+  
+    it('renders restaurants from local storage', async () => {
+      localStorage.setItem('beenToRestaurants', JSON.stringify(['1', '2']));
+      fetch
+        .mockResponseOnce(JSON.stringify({ Name: 'Restaurant 1'}))
+        .mockResponseOnce(JSON.stringify({ Name: 'Restaurant 2'}));
+  
+      render(
+        <Router>
+          <BeenToList />
+        </Router>
+      );
+  
+      await waitFor(() => {
+        expect(screen.getByText('Restaurant 1')).toBeInTheDocument();
+        expect(screen.getByText('Restaurant 2')).toBeInTheDocument();
+      });
+    });
+  
+    it('deletes a restaurant from the list', async () => {
+      localStorage.setItem('beenToRestaurants', JSON.stringify(['1']));
+      fetch.mockResponseOnce(JSON.stringify({ Name: 'Restaurant 1'}));
+  
+      render(
+        <Router>
+          <BeenToList />
+        </Router>
+      );
+  
+      await waitFor(() => {userEvent.click(screen.getByText('Delete'));});
+      await waitFor(() => expect(screen.queryByText('Restaurant 1')).not.toBeInTheDocument());
+    });
+  
+    it('navigates to restaurant page on "View" click', async () => {
+      localStorage.setItem('beenToRestaurants', JSON.stringify(['1']));
+      fetch.mockResponseOnce(JSON.stringify({ Name: 'Restaurant 1'}));
+  
+      render(
+        <Router>
+          <BeenToList />
+        </Router>
+      );
+
+      await waitFor(() => {userEvent.click(screen.getByText('View'));});
+      await waitFor(() => {expect(mockedNavigate).toHaveBeenCalledWith('/restaurant/1');});
+    });
+  });
